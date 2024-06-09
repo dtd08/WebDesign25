db64f64934b66c1afa2d8b8f615d603d2cd329de
dtd08 <126937437+dtd08@users.noreply.github.com>
2024-06-10 08:36:41 +0900

    Add files via upload

---
+++ Menu/Menu_04/script.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+    let navList = document.querySelectorAll(".nav > ul > li");
+
+    navList.forEach(navItem => {
+        navItem.addEventListener("mouseover", function() {
+            this.querySelector(".submenu").style.height = "165px";
+        });
+    });
+
+    navList.forEach(navItem => {
+        navItem.addEventListener("mouseout", function() {
+            this.querySelector(".submenu").style.height = "0px";
+        });
+    });
+}
