8ebcbbac7729f12ede9760d276797d258d414428
dtd08 <126937437+dtd08@users.noreply.github.com>
2024-06-11 17:27:56 +0900

    Update script.js

--- Menu/Menu_06/script.js
+++ Menu/Menu_06/script.js
@@ -1,3 +1,17 @@
 window.onload = function() {
-    
+    let navList = document.querySelector(".nav > ul");
+
+    navList.addEventListener("mouseover", () => {
+        navList.querySelectorAll(".submenu").forEach(sub => {
+            sub.style.display = "block";
+        });
+        document.getElementById("main").classList.add("on");
+    });
+
+    navList.addEventListener("mouseout", () => {
+        navList.querySelectorAll(".submenu").forEach(sub => {
+            sub.style.display = "none";
+        });
+        document.getElementById("main").classList.remove("on");
+    });
 }
