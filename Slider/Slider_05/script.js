ce49d98dfe858fababaa2d45490a97ecb6127eae
dtd08 <126937437+dtd08@users.noreply.github.com>
2024-06-17 08:42:34 +0900

    Add files via upload

---
+++ Slider/Slider_05/script.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+    let currentIndex = 0;
+    const sliderWrap = document.querySelector(".sliderWrap");
+    const slider = document.querySelectorAll(".slider");
+    const sliderClone = sliderWrap.firstElementChild.cloneNode(true);
+    sliderWrap.appendChild(sliderClone);
+
+    setInterval(() => {
+        currentIndex++;
+        sliderWrap.style.transition = "all 0.6s";
+        sliderWrap.style.marginLeft = -currentIndex * 100 + "px";
+
+        if (currentIndex == slider.length) {
+            setTimeout(() => {
+                sliderWrap.style.transition = "0s";
+                sliderWrap.style.marginLeft = "0";
+                currentIndex = 0;
+            }, 700);
+        }
+    }, 3000);
+};
