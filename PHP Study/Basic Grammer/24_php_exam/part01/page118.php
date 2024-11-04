f08aaa8af19d3914a1fdfef423cc057da84b2773
LEE SEOYOUNG <126937437+dtd08@users.noreply.github.com>
2024-11-05 08:29:42 +0900

    Update page118.php

--- PHP Study/Basic Grammer/24_php_exam/part01/page118.php
+++ PHP Study/Basic Grammer/24_php_exam/part01/page118.php
@@ -7,7 +7,15 @@
 </head>
 <body>
     <?php
-        $weather = 
+        $weather = "sunny";
+        $friendsInHome = false;
+
+        if ($weather == "rainning") {
+            echo "집에서 잠이나 자자..";
+        }
+        elseif ($friendsInHome) {  // php의 elseif문은 'else if' 여도 되고 'elseif' 여도 된다. 다만 한가지로 통일하여 사용하자.
+            echo "친구 집으로 놀러가자!";
+        } else echo "에효.. 집에서 놀자";
     ?>
 </body>
 </html>
