c9f08c38a7c78a158a651a0502c333ccc724c15c
LEE SEOYOUNG <126937437+dtd08@users.noreply.github.com>
2024-11-15 08:27:48 +0900

    24.11.08

--- PHP Study/Basic Grammer/24_php_exam/part01/page118.php
+++ PHP Study/Basic Grammer/24_php_exam/part01/page118.php
@@ -16,8 +16,6 @@
         elseif ($friendsInHome) {  // php의 elseif문은 'else if' 여도 되고 'elseif' 여도 된다. 다만 한가지로 통일하여 사용하자.
             echo "친구 집으로 놀러가자!";
         } else echo "에효.. 집에서 놀자";
-
-    // 24.11.08
     ?>
 </body>
 </html>
